ae26d63fb678b924ecf1ec7a10ae3d9ac53c4e51
kishore <kishorekannan63@gmail.com>
2022-01-07 18:34:12 +0530

    added routing

---
+++ server/routes/routes.js
@@ -0,0 +1,5 @@
+const router=require('express').Router();
+
+module.exports=router.get('/register',(req,res)=>{
+    res.send("hello register");
+})
--- server/server.js
+++ server/server.js
@@ -5,7 +5,9 @@ const port=5000 ||process.env.port;
 
 require('./models/db');
 
+const router=require('./routes/routes')
 
+app.use('/api',router);
 
 app.use('/',(req,res)=>{
     res.send('hello world');
