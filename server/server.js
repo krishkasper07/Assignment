a31a90a617d273cdd6fac9c158528fcb151dcd13
kishore <kishorekannan63@gmail.com>
2022-01-08 00:40:04 +0530

    done register api

--- server/server.js
+++ server/server.js
@@ -1,15 +1,20 @@
 const express=require('express');
+
 const app=express();
 
 const port=5000 ||process.env.port;
+
+const router=require('./routes/userRouter');
+
+const morgan=require('morgan');
 
 require('./models/db');
 
 app.use(express.json());
 
-const router=require('./routes/userRouter');
-
 app.use('/api',router);
+
+app.use(morgan('dev'));
 
 app.listen(port,()=>{
     console.log(`listenting to port ${port}`)
