2853cb518ae9d5887204afa8cff1adb754c3c52e
kishore <kishorekannan63@gmail.com>
2022-01-09 13:47:24 +0530

    done register and login with authentication

--- client/src/login/login.jsx
+++ client/src/login/login.jsx
@@ -1,16 +1,56 @@
 import "./login.css"
 import { Link } from "react-router-dom";
-const Login = () => {
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { toast } from "react-toastify";
+import axios from "axios";
+const Login = (props) => {
+  const formik=useFormik({
+    initialValues:{
+      email:'',
+      password:'',
+    },
+    validationSchema:yup.object({
+      email:yup.string()
+      .strict()
+      .trim()
+      .email('Enter vaild Email Address')
+      .required('Email is required*'),
+      password:yup.string()
+      .strict()
+      .required('Password is required*'),
+    }),
+    onSubmit:(userdata)=>{
+             console.log(userdata);
+             axios.post('http://localhost:5000/api/login',userdata)
+             .then(res=>{
+               localStorage.setItem('auth',JSON.stringify(res.data));
+               toast.success(`sucessfully logged in ${userdata.email}`);
+               props.history.push('/uploader')
+             })
+             .catch(err=>{
+               toast.error(err.response.data)
+             })
+    }
+  })
     return ( <div className="login">
-        <form className="loginform">
+        <form className="loginform" onSubmit={formik.handleSubmit}>
         <h1>Login</h1>
         <div className="mb-3">
           <label>Email address</label>
-          <input type="email" className="form-control"/>
+          <input type="email" className="form-control"
+           name="email"
+           autoComplete="off"
+           onChange={formik.handleChange}
+           value={formik.values.email}/>
           </div>
         <div className="mb-3">
           <label>Password</label>
-          <input type="text" className="form-control"/>
+          <input type="password" className="form-control" 
+          name='password'
+          autoComplete="off"
+          onChange={formik.handleChange}
+          value={formik.values.password}/>
         </div>
         <button type="submit" className="btn btn-primary">Submit</button>
         <div className="registeruser">
--- client/src/protectedRouter/protected.jsx
+++ client/src/protectedRouter/protected.jsx
@@ -2,10 +2,12 @@ import { Redirect, Route } from "react-router-dom";
 
 const ProtectedRouter = ({component,...rest}) => {
     let RenderComponent=component;
+    let hasToken=JSON.parse(localStorage.getItem('auth'));
+
     return (<Route 
        {...rest}
         render={props=>{
-            return false ?(<RenderComponent {...props}/>) :(<Redirect to={{pathname:'login'}}/>)
+            return hasToken !== null && hasToken !=='' ?(<RenderComponent {...props}/>) :(<Redirect to={{pathname:'login'}}/>)
         }}
     />);
 }
