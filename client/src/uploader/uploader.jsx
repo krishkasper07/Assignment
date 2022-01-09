3c8bbbf7c0dc43d163f08aae6901ef4555c55b46
kishore <kishorekannan63@gmail.com>
2022-01-09 16:20:15 +0530

    done changes on uploader component

--- client/src/uploader/uploader.jsx
+++ client/src/uploader/uploader.jsx
@@ -1,5 +1,21 @@
+import './uploader.css'
 const Uploader = () => {
-    return ( 'uploader' );
+    const handleChange=(e)=>{
+        console.log(e.target.files);
+    }
+    return ( <div>
+        <div className="uploader">
+        <input
+            type='file'
+            className='uploadinput'
+            id='customFile'
+            accept="application/JSON"
+            onChange={handleChange}
+          />
+        <button>submit</button>
+       </div>
+       </div>
+     );
 }
  
 export default Uploader;
